23f48a8e569ce6d1b4186a23cf7388e15d5b146c
eritten <eritten2@gmail.com>
2022-10-22 20:22:46 -0700

    adding static and media settings

--- imex_project/urls.py
+++ imex_project/urls.py
@@ -18,5 +18,5 @@ path('api/', include(review.urls)),
 path('agents/', agents, name='agents'),
 path('admin/', admin.site.urls),
 ]
-#urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-#urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

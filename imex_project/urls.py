70c64c6cc7d3124c651a8e5636f0b0e6ea1b79d9
eritten <eritten2@gmail.com>
2022-10-22 19:46:29 -0700

    media and static files  serving configuration

--- imex_project/urls.py
+++ imex_project/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from imex_app.views import agents,create_user
 from imex_app.api import ReviewResource as rr
+from django.conf.urls.static import static
+#from django.conf import settings
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -16,3 +18,5 @@ path('api/', include(review.urls)),
 path('agents/', agents, name='agents'),
 path('admin/', admin.site.urls),
 ]
+#urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+#urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

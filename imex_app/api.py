74e00ce31dc6c19360a91abe342efee318d300d0
Gafaru Manyea <manyeagaf@gmail.com>
2022-10-24 21:48:34 +0000

    ok

--- imex_app/api.py
+++ imex_app/api.py
@@ -1,5 +1,5 @@
 from tastypie.resources import ModelResource, ALL, ALL_WITH_RELATIONS
-#from .models import Agent, Review
+from .models import Agent, Review
 from tastypie import fields
 
 #class AgentResource(ModelResource):
@@ -17,3 +17,17 @@ from tastypie import fields
 #        authorization = Authorization()
 #        filtering = {'agent': ALL_WITH_RELATIONS, 'name': ALL, 'rating':ALL, 'content':ALL}
 
+class AgentResource(ModelResource):
+    class Meta:
+        queryset = Agent.objects.all()
+        resource_name = 'agent'
+        filtering = {'id': ALL, 'name': ALL, 'telephone_number': ALL}
+
+class ReviewResource(ModelResource):
+    agent = fields.ForeignKey(AgentResource, 'agent', full=True)
+    class Meta:
+        queryset = Review.objects.all()
+        resource_name = 'review'
+#        allow_methods = ['GET', 'POST']
+#        authorization = Authorization()
+        filtering = {'agent': ALL_WITH_RELATIONS, 'name': ALL, 'rating':ALL, 'content':ALL}

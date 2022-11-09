6bedbaad5c79f4d33067d2f69ced6075564c7f59
eritten <eritten2@gmail.com>
2022-11-09 17:46:16 +0000

    adding email authentication backend to the username authentication backend so that a client or an agent can login by email or username

--- imex_project/settings.py
+++ imex_project/settings.py
@@ -132,3 +132,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend',
+'imex_app.email_auth_middleware.EmailBackend']
+

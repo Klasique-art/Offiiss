809ea945fbc8ee167e5b1246416d127a598267c1
eritten <eritten2@gmail.com>
2022-11-17 17:17:44 +0000

    fixing static files serving error

--- imex_project/urls.py
+++ imex_project/urls.py
@@ -33,5 +33,6 @@ path('order/done/', done, name='order_done'),
 path("fetch-orders/", orders, name='orders'),
 path('order/', order, name='order'),
 ]
-urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns+= static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

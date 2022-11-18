e23c141653daac054e6040988c6835211ed29ba0
= <=>
2022-11-18 21:22:38 +0100

    continued the design. added new features on the ui

--- home/static/js/main.js
+++ home/static/js/main.js
@@ -1,8 +1,15 @@
-window.addEventListener("load", ()=>{
-    // welcome notice
-    const welNote = document.querySelector(".wel-notice")
+window.addEventListener("load", () => {
 
-    setTimeout(() => {
-        welNote.classList.add("go")
-    }, 2000);
-})
+  // welcome notice
+  const welNote = document.querySelector(".wel-notice");
+
+  setTimeout(() => {
+    welNote.classList.add("go");
+  }, 1000);
+
+  // making the navbar sticky
+  const navbar = document.querySelector(".nav-wrapper");
+  window.addEventListener("scroll", () => {
+    navbar.classList.toggle("sticky", scrollY > 70);
+  });
+});

bbbef37f9a2cb926bebe9d8abdcd9dcf8b736ece
eritten <eritten2@gmail.com>
2022-11-18 14:39:31 +0000

    separating imex_app urls from the main urls. every imex url will be offis-api/<agents or any end point>

---
+++ imex_app/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path
+from . views import MyTokenObtainPairView, agents,create_user,MyTokenObtainPair,reviews, create_agent, change_password, change_email, change_username, orders
+# from imex_app.api import ReviewResource as rr
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+from . api import ProfileView, ImageView
+from  .order import check_code, order, done
+
+urlpatterns = [
+path('profile/<int:pk>/', ProfileView.as_view({"post": "update"}), name='profile'),
+path("image/<int:pk>/", ImageView.as_view({"post": "update"}), name="image"),
+path('create-user/', create_user, name='create_user'),
+path('create-agent/', create_agent, name='create_agent'),
+path('change-username/', change_username, name='change_username'),
+path('change-email/', change_email, name='change_email'),
+path('change-password/', change_password, name='change_password'),
+path('login/', MyTokenObtainPairView.as_view(), name='token_obtain_pair'),
+path('refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+path('agents/', agents, name='agents'),
+path('reviews/',reviews,name = 'reviews' ),
+path('order/check-code/', check_code, name='check_code'),
+path('order/done/', done, name='order_done'),
+path("fetch-orders/", orders, name='orders'),
+path('order/', order, name='order'),
+]

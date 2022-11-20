c3bae7ef3010ebbde6b54f6c014f77fba24cdc94
eritten <eritten2@gmail.com>
2022-11-20 12:36:58 +0000

    seven changed

--- imex_project/urls.py
+++ imex_project/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 from home.views import terms, privacy
-from home.blog_views import home_page, letter, search
+from home.blog_views import home_page, letter, search, detail
 from home import feeds
 from django.contrib.sitemaps.views import sitemap # new
 from home.sitemap import BlogSitemap
@@ -12,6 +12,7 @@ maps = {"blog": BlogSitemap}
 
 
 urlpatterns = [
+path('detail/<int:pk>/<slug:slug>/', detail, name='detail'),
     path('feed/', feeds.LatestPostsFeed(), name='feed'),
 path('search/', search, name='search'),
 path('letter/', letter, name='letter'),

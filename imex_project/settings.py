d7581ec2f849468968cb134219791a818495f113
eritten <eritten2@gmail.com>
2022-11-24 14:17:53 +0000

    adding compressing

--- imex_project/settings.py
+++ imex_project/settings.py
@@ -62,6 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+STATICFILES_STORAGE = "whitenoise.storage.CompressedStaticFilesStorage"
 
 ROOT_URLCONF = 'imex_project.urls'
 TEMPLATES = [
